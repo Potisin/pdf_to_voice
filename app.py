cc921cb49b8d0b90ef685c32dcef150053eaeaa0
potisin <potisin@outlook.com>
2023-06-01 17:43:53 +0300

    done

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
 from flask import Flask, render_template, request
 
+from converter import converter
+
 app = Flask(__name__)
+
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'GET':
-        print("GET")
+    error = None
     if request.method == 'POST':
-        rq = request
-        print("POST")
-        print(rq.data)
-        # file = request.files['u_img']
-    return render_template('index.html')
+        file = request.files['file']
+        if file.filename == '':
+            error = 'No selected file'
+            return render_template('index.html', error=error)
+        if file.filename.rsplit('.', 1)[1].lower() != 'pdf':
+            error = "It's not .pdf file"
+            return render_template('index.html', error=error)
+        mp3 = converter(file)
+
+    return render_template('index.html', error=error)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
--- converter.py
+++ converter.py
@@ -2,5 +2,14 @@ from gtts import gTTS
 import pdfplumber
 
 
-def converter(file, language='en'):
-    with pdfplumber.PDF()) as pdf:
+def converter(file, language='ru'):
+    with pdfplumber.open(file) as pdf:
+        pages = [page.extract_text() for page in pdf.pages]
+    text = ''.join(pages)
+    text = text.replace('\n', '')
+    voice = gTTS(text=text, lang=language, slow=False)
+    voice.save(f'{file.filename}.mp3')
+    return voice
+
+def test():
+    pass
